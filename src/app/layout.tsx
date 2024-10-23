c6ce3183844cd9a4eafb6c7f5c3795c1106204d5
Ed <odesey@hotmail.com>
2024-10-22 22:33:55 -0400

    and another import error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Header } from "@/sections/Header";
 import { Features } from "@/sections/Features";
 import { NewHero } from "@/sections/NewHero";
 import { NewNav } from "@/sections/NewNav";
-import { ImageSlider } from "@/sections/ImageSlider";
+// import { ImageSlider } from "@/sections/ImageSlider";
 import { ScreenShots } from "@/sections/ScreenShots";
 import Navbar from "@/components/layout/Navbar";
 import { ThemeProvider } from "@/app/theme-provider";
