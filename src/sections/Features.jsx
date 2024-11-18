c72a7ef326a50f7af02daece9c500ac3375a89ff
Dev <dev@genapp.com>
2024-11-17 22:38:36 -0500

    added tab buttons for feature section

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -16,6 +16,8 @@ import Members from "@/assets/users.svg";
 import Messages from "@/assets/messages.svg";
 import Permissions from "@/assets/users-gear.svg";
 import Plate from "@/assets/plate-utensils.svg";
+import ChipTabs from "@/components/ChipTabs";
+import { FiSend } from "react-icons/fi";
 import { useSectionInView } from "@/lib/hooks";
 
 const ICON_CLASS = "size-10 md:size-12 inline-flex justify-center items-center";
@@ -95,14 +97,32 @@ const features = [
   }
 ];
 
+const FEATURE_TABS = [
+  {
+    id: 0,
+
+    title: "Congregation"
+  },
+  {
+    id: 1,
+
+    title: "Community"
+  },
+  {
+    id: 2,
+    title: "Churches"
+  }
+];
 export const Features = () => {
+  const [selected, setSelected] = useState(0);
+
   const { ref } = useSectionInView("#features");
 
   return (
     <section ref={ref} className="relative scroll-mt-24 mb-12" id="features">
       <div className="container">
         <div
-          className="text-center max-w-3xl lg:mx-auto"
+          className="text-center "
           data-aos="fade-up"
           data-aos-duration="2000"
         >
@@ -110,6 +130,19 @@ export const Features = () => {
           <p className="text-onyx-300 mt-4">
             Here are some of the features that GenesisApp offers
           </p>
+          <div className="w-full flex relative items-center justify-center align-middle">
+            <div className=" min-h-[200px] flex items-center justify-center w-full">
+              {FEATURE_TABS.map((tab) => (
+                <NeumorphismButton
+                  key={tab.id}
+                  label={tab.title}
+                  selected={selected}
+                  setSelected={setSelected}
+                  id={tab.id}
+                />
+              ))}
+            </div>
+          </div>
         </div>
         <div className="grid md:grid-cols-2 gap-6 lg:grid-cols-3 lg:gap-x-6 mt-24 lg:gap-y-4 lg:text-center mx-8">
           {features.map((feature) => (
@@ -147,5 +180,36 @@ export const Features = () => {
         </div>
       </div>
     </section>
+  );
+};
+// shadow-[-5px_-5px_10px_rgba(255,_255,_255,_0.8),_5px_5px_10px_rgba(0,_0,_0,_0.25)]
+
+const NeumorphismButton = ({ label, selected, setSelected, id }) => {
+  return (
+    <button
+      onClick={() => setSelected(id)}
+      className={`
+        mx-1 md:mx-4 lg:mx-10
+        px-4 py-2 rounded-[8px]
+        flex items-center gap-2
+        dark:text-onyx-300 text-onyx-500
+        border border-white dark:border-onyx-800
+        transition-all ease-in-out duration-200
+        ${
+          selected === id
+            ? "shadow-[-1px_-1px_5px_rgba(255,_255,_255,_0.6),_1px_1px_5px_rgba(40,_149,_255,_0.3),inset_-2px_-2px_5px_rgba(255,_255,_255,_1),inset_2px_2px_4px_rgba(40,_149,_255,_0.3)]"
+            : "shadow-[-4px_-4px_8px_rgba(255,_255,_255,_0.8),_4px_4px_8px_rgba(40,_149,_255,_0.3)]"
+        }
+    `}
+    >
+      {/* <FiSend /> */}
+      <p
+        className={`text-sm tracking-tighter md:tracking-wide lg:text-2xl lg:p-2 min-w-16 md:min-w-24  ${
+          selected === id ? "font-bold text-primeBlue-400" : "font-semibold"
+        }`}
+      >
+        {label}
+      </p>
+    </button>
   );
 };
