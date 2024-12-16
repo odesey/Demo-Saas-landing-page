a1aa98d4b8b636754ad78e94bfe7b3689658e119
Dev <dev@genapp.com>
2024-12-16 02:28:35 -0500

    adjusted card icon colors and refactored privacy section

--- src/sections/Privacy.jsx
+++ src/sections/Privacy.jsx
@@ -38,40 +38,94 @@ const Privacy = () => {
   return (
     <section className="overflow-hidden py-60 max-w-full md:px-4 ">
       <div className="relative z-0 flex-1 items-center justify-center align-middle">
-        <div className="-z-10 max-w-screen-lg w-full h-2/4 blur-2xl absolute">
-          <div className="absolute top-24 left-24 w-56 h-56 bg-violet-500 rounded-full mix-blend-multiply opacity-70 animate-blob filter blur-3xl"></div>
+        <div className="-z-10 max-w-screen-lg w-full h-2/4 blur-2xl absolute top-52">
+          <div className="absolute left-24 w-56 h-56 bg-violet-500 rounded-full mix-blend-multiply opacity-70 animate-blob filter blur-3xl"></div>
           <div className="absolute hidden md:block bottom-2 right-1/4 w-56 h-56 bg-sky-500 rounded-full mix-blend-multiply opacity-70 animate-blob delay-1000 filter blur-3xl"></div>
           <div className="absolute hidden md:block bottom-1/4 left-1/3 w-56 h-56 bg-pink-500 rounded-full mix-blend-multiply opacity-70 animate-blob delay-500 filter blur-3xl"></div>
         </div>
         <div className=" lg:mx-auto lg:max-w-6xl">
-          <div className="mb-16 flex justify-between md:gap-4">
+          <div className="flex justify-between md:gap-4 flex-col">
             <div className="flex-1 items-center align-middle relative flex">
-              <h2 className="text-3xl font-bold leading-[1.2] md:text-5xl left-[2vw] md:left-0 relative">
+              <h2 className="text-4xl font-bold leading-[1.2] md:text-5xl left-[2vw] md:left-0 relative">
                 Church Privacy,{" "}
-                <span className="text-onyx-400">is Our Priority!</span>
+                <span className="text-onyx-400">
+                  {/* <br /> */}
+                  is Our Priority!
+                </span>
               </h2>
             </div>
+            <div className="justify-end relative flex flex-row flex-1 align-middle items-center my-8">
+              <div className="flex relative items-center align-middle ">
+                <div
+                  className={`
+                ${position === 0 ? "opacity-0" : ""}
+        backdrop-blur-sm backdrop-filter
+        items-center justify-center flex size-14 md:size-16 lg:size-20
+        rounded border border-white/75 dark:border-onyx-700 bg-primeBlue-50
+        dark:bg-onyx-950 dark:bg-[linear-gradient(135deg,_rgba(31,30,38,0.2),_rgba(75,72,91,0.8))]
+        bg-[linear-gradient(135deg,_rgba(158,207,255,0.15),_rgba(255,255,255,0.4))]
+        [box-shadow:4px_4px_8px_rgba(158,207,255,0.3),_-4px_-4px_8px_#ffffff]
+        dark:[box-shadow:6px_6px_12px_rgba(0,0,0,0.1),_-6px_-6px_12px_rgba(0,0,0,0.01)]
+        `}
+                >
+                  <div className="fill-primeBlue-500 backdrop-blur-sm backdrop-filter">
+                    <button
+                      onClick={shiftLeft}
+                      className="align-middle relative"
+                    >
+                      <Prev
+                        className={`size-8 md:size-10 lg:size-12 inline-flex justify-center items-center ease-in-out transition-opacity duration-300
+                        ${position === 0 ? "opacity-0 cursor-default" : ""}
+                        `}
+                      />
+                    </button>
+                  </div>
+                </div>
+              </div>
 
-            <div className="flex relative justify-self-end items-center align-middle">
-              <button onClick={shiftLeft}>
+              <div className="flex relative items-center align-middle ml-8">
+                {/* <button onClick={shiftLeft}>
                 <Prev
                   className={`md:size-12 lg:mx-6 lg:size-16 size-12 fill-primeBlue-500 z-50 dark:fill-onyx-600 ease-in-out transition-opacity duration-300 ${
                     position === 0 ? "opacity-0" : ""
                   }`}
                 />
-              </button>
+              </button> */}
 
-              <button onClick={shiftRight}>
-                <Next
-                  className={`md:size-12 lg:ml-6 lg:size-16 size-12 fill-primeBlue-500 z-50 dark:fill-onyx-600 ease-in-out transition-opacity duration-300 ${
-                    position === FEATURES.length - 1 ? "opacity-0" : ""
-                  }`}
-                />
-              </button>
+                <div
+                  className={`
+                  ${position === FEATURES.length - 1 ? "opacity-0" : ""}
+        backdrop-blur-sm backdrop-filter
+        items-center justify-center flex size-14 md:size-16 lg:size-20
+        rounded border border-white/75 dark:border-onyx-700 bg-primeBlue-50
+        dark:bg-onyx-950 dark:bg-[linear-gradient(135deg,_rgba(31,30,38,0.2),_rgba(75,72,91,0.8))]
+        bg-[linear-gradient(135deg,_rgba(158,207,255,0.15),_rgba(255,255,255,0.4))]
+        [box-shadow:4px_4px_8px_rgba(158,207,255,0.3),_-4px_-4px_8px_#ffffff]
+        dark:[box-shadow:6px_6px_12px_rgba(0,0,0,0.1),_-6px_-6px_12px_rgba(0,0,0,0.01)]
+        `}
+                >
+                  <div className="fill-primeBlue-500 backdrop-blur-sm backdrop-filter">
+                    <button
+                      onClick={shiftRight}
+                      className="align-middle relative"
+                    >
+                      <Next
+                        className={`size-8 md:size-10 lg:size-12 inline-flex justify-center items-center ease-in-out transition-opacity duration-300
+                        ${
+                          position === FEATURES.length - 1
+                            ? "opacity-0 cursor-default"
+                            : ""
+                        }
+                        `}
+                      />
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
 
-          <div className="flex gap-4 mt-8 relative left-[2vw] md:left-0">
+          <div className="flex gap-4 relative left-[2vw] md:left-0">
             {FEATURES.map((feat, index) => (
               <Feature
                 {...feat}
@@ -126,7 +180,7 @@ const Feature = ({ position, index, title, description, Icon }) => {
       }
       `}
           >
-            <div className="fill-primeBlue-500 backdrop-blur-sm backdrop-filter">
+            <div className="fill-primeBlue-300 backdrop-blur-sm backdrop-filter">
               {Icon}
             </div>
           </div>
