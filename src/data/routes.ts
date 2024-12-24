ae66fcdf0cf523ee683b09e9a55f92adf78915c7
Dev <dev@genapp.com>
2024-12-23 20:58:34 -0500

    removed bad import

--- src/data/routes.ts
+++ src/data/routes.ts
@@ -1,32 +1,22 @@
-// import { BiHomeAlt2 } from "react-icons/bi";
-// import { FiSearch } from "react-icons/fi";
-// import { PiChatCircleBold } from "react-icons/pi";
-// import { IoPricetagsOutline } from "react-icons/io5";
-
 export const routes = [
   {
     title: "Features",
     href: "#"
-    // Icon: BiHomeAlt2,
   },
   {
     title: "Screenshots",
     href: "#"
-    // Icon: FiSearch,
   },
   {
     title: "Download",
     href: "#"
-    // Icon: PiChatCircleBold,
   },
   {
     title: "F.A.Q",
     href: "#"
-    // Icon: IoPricetagsOutline,
   },
   {
     title: "Contact",
     href: "#"
-    // Icon: PiChatCircleBold,
   }
 ];
